e351c2c0d723ca27830ddd4d0c25f70b69409bb6
ntzwilly <ntzwilly@gmail.com>
2023-01-31 20:00:52 +0200

    create main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/main.css'
+import store from './assets/store'
+
+let localAuthToken = localStorage.auth_token;
+let cookiesExists = localAuthToken !== 'undefined' && localAuthToken !== null;
+if(cookiesExists) {
+  const auth_token = localStorage.getItem('auth_token');
+  const authTokenExists = auth_token !== 'undefined' && auth_token !== null
+  if(authTokenExists) {
+    store.dispatch("loginUserWithToken", { auth_token });
+  }
+}
+
 
 createApp(App).mount('#app')
