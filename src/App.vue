8a864811b7b9402be728271ad1507c2e92945729
ntzwilly <ntzwilly@gmail.com>
2023-01-31 20:00:32 +0200

    create App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <SessionManager />
 </template>
+
+// Path: src/components/SessionManager.vue
+<script>
+  import SessionManager from './components/SessionManager.vue'
+
+  export default {
+    name: 'SessionManager',
+    components: {
+      SessionManager
+    }
+  }
+</script>
 
 <style scoped>
 header {
